1866ca712b85da350ac2fc181fe44027bb987c44
Sean Cone <seandoesitbetter@gmail.com>
2020-07-18 13:08:48 -0700

    Fixing formatting and badge issues

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
-# README Generator
-    ![githublicense](https://img.shields.io/badge/license-MIT-blue.svg)
-    ## Description
-    It generates README files.
-    ## Table of Contents
-    [Questions](#Questions)
-    [Installation](#Installation)
-    [Usage](#Usage)
-    [License](#License)
-    [Contributing](#Contributing)
-    [Tests](#Tests)
-    ## Questions
-        Feel free to email me at seandoesitbetter@gmail.com with any questions.
-        [seanc0ne](https://www.github.com/seanc0ne)
-    ## Installation
-        npm
-    ## Usage
-        npm stuff
-    ## License 
-        This application is covered under the MIT license.
-    ## Contributing
-        I don't know.
-    ## Tests
-        Party.
-  
+
+# party
+![githublicense](https://img.shields.io/badge/license-APACHE%202.0-blue.svg)
+## Description
+party
+## Table of Contents
+[Questions](#Questions)
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contributing)
+[Tests](#Tests)
+## Questions
+Feel free to email me at party with any questions.
+[party](https://www.github.com/party)
+## Installation
+party
+## Usage
+party
+## License
+This application is covered under the APACHE 2.0 license.
+## Contributing
+party
+## Tests
+party
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,34 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
-    ${renderLicenseBadge(data.license)}
-    ## Description
-    ${data.description}
-    ## Table of Contents
-    [Questions](#Questions)
-    [Installation](#Installation)
-    [Usage](#Usage)
-    [License](#License)
-    [Contributing](#Contributing)
-    [Tests](#Tests)
-    ## Questions
-        Feel free to email me at ${data.email} with any questions.
-        [${data.github}](https://www.github.com/${data.github})
-    ## Installation
-        ${data.installation}
-    ## Usage
-        ${data.usage}
-    ## License 
-        ${explainLicense(data.license)}
-    ## Contributing
-        ${data.contributing}
-    ## Tests
-        ${data.tests}
-  `;
-  }
+return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+[Questions](#Questions)
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contributing)
+[Tests](#Tests)
+## Questions
+Feel free to email me at ${data.email} with any questions.
+[${data.github}](https://www.github.com/${data.github})
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${explainLicense(data.license)}
+## Contributing
+${data.contributing}
+## Tests
+${data.tests}
+`;
+}
 
 function renderLicenseBadge(license) {
-    if (license !== 'None') {
+    if (license === 'APACHE 2.0' || license === 'GPL 3.0' || license === 'BSD 3') {
+        let licenseSplit = license.split(' ');
+        return `![githublicense](https://img.shields.io/badge/license-${licenseSplit[0]}%20${licenseSplit[1]}-blue.svg)`;
+    }
+    if (license === 'MIT') {
         return `![githublicense](https://img.shields.io/badge/license-${license}-blue.svg)`;
     } else {
         return '';
